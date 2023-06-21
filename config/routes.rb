e6599a702aed0d8c1103adb3b6ac90db2a2984ee
zunair <zunair.dev@gmail.com>
2023-06-21 18:08:07 +0500

    set root path

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  root 'rooms#index'
+
   resources :rooms do
     resources :messages
   end
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
